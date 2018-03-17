ba0cd2e2bd658ae7e83ae13d850807d8fdd7259c
Duc Anh Nguyen <duc4nh@Ducs-MacBook-Pro.local>
2018-03-17 19:21:38 +0700

    feat/ team controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,8 +4,18 @@ class TeamsController < ApplicationController
   end
   
   def new
+    @team = Team.new
   end
 
   def create
+    team = Team.new(params.require(:team).permit(:name))
+    player_ids = params[:player_id].uniq.reject{ |c| c.empty? }
+    team.players = Player.find(player_ids)
+
+    if team.save
+      redirect_to teams_path, notice: 'Successful !'
+    else
+      redirect_to new_team_path, alert: "Error: #{team.errors.full_messages.join(', ')}"
+    end
   end
 end
